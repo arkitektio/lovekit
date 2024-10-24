e4985dc6cf3e1efa68149caead0b803f26e18612
Johannes Roos <jhnnsrs@gmail.com>
2024-10-24 17:26:28 +0200

    fix new requirements

--- lovekit/__arkitekt__.py
+++ lovekit/__arkitekt__.py
@@ -1,14 +1,14 @@
-from arkitekt_next.model import Manifest
+from arkitekt_next.base_models import Manifest
 from koil.composition.base import KoiledModel
 from herre import Herre
 from fakts import Fakts
 
 from arkitekt_next.service_registry import Params
-from arkitekt_next.model import Requirement
+from arkitekt_next.base_models import Requirement
 
 
 def init_services(service_builder_registry):
-   
+
     class ArkitektNextLovekit(KoiledModel):
         async def __aenter__(self):
             return self
@@ -25,6 +25,7 @@ def init_services(service_builder_registry):
         "livekit",
         build_arkitekt_lovekit,
         Requirement(
+            key="livekit",
             service="io.livekit.livekit",
             description="An instance of ArkitektNext Lok to authenticate the user",
         ),
